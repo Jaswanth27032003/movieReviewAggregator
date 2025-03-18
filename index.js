c976efcaff38f8609815dc8b7c6ab6fbc02b3e5d
Jaswanth <121560002+Jaswanth27032003@users.noreply.github.com>
2025-03-18 11:40:43 +0530

    rename User.js to user.js and fix the path in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
 
-const user = require('./models/User');
+const user = require('./models/ser');
 // Load environment variables
 dotenv.config();
 
