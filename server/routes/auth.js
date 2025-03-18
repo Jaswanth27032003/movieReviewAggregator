32640784c76614fb0d42c6c52dc8a11af55e5966
Jaswanth <121560002+Jaswanth27032003@users.noreply.github.com>
2025-03-18 12:13:17 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/user'); // Importing User model
+const User = require('../models/User'); // Importing User model
 
 router.post('/login', async (req, res) => {
     try {
